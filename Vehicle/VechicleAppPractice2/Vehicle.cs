ddcedf17ebdc15a302813afbb35c476b9c1568fc
sohelrkhan <55046835+sohelrkhan@users.noreply.github.com>
2019-10-18 15:33:13 +0600

    Assignment10

---
+++ Vehicle/VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+   public class Vehicle
+    {
+        public string vehiclename { set; get; }
+        public int regno { set; get; }
+        public  List<Double> speed = new List<double>();
+        public Vehicle(string vehiclename,int regno)
+        {
+            this.vehiclename = vehiclename;
+            this.regno = regno;
+        }
+    }
+}
---
+++ Vehicle/VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+
+        public Vehicle Vehicle;
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            Vehicle  = new Vehicle(vehicleNameTextBox.Text,Convert.ToInt32(regNoTextBox.Text));
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            Vehicle.speed.Add(Convert.ToDouble(speedTextBox.Text));
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            maxSpeedTextBox.Text = Vehicle.speed.Max().ToString();
+            minSpeedTextBox.Text = Vehicle.speed.Min().ToString();
+            averageSpeedTextBox.Text = Vehicle.speed.Average().ToString();
+        }
+    }
+}
